9bb013a49bf0ef73ca48c0435d5829e09b5ed52a
BuildTools <unconfigured@null.spigotmc.org>
2024-03-28 09:53:48 +0200

    Ikäkone

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Kuinka vanha olet?");
+        int ika = Integer.valueOf(lukija.nextLine());
+
+        if (ika > 0 && ika < 120) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Mahdotonta!");
+        }
     }
 }
