8b594e6b44706483a8b26d0a6ef1a1c042b83493
BuildTools <unconfigured@null.spigotmc.org>
2024-03-28 20:03:12 +0200

    Lahjaverolaskuri

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
 
-        System.out.println("Kuinka vanha olet?");
-        int ika = Integer.valueOf(lukija.nextLine());
+        System.out.println("Lahjan suuruus?");
+        int lahja = Integer.valueOf(lukija.nextLine());
 
-        if (ika > 0 && ika < 120) {
-            System.out.println("OK");
+        if (lahja < 5000) {
+            System.out.println("Ei veroa!");
+        } else if (lahja >= 5000 && lahja < 25000) {
+            double vero = 100 + (lahja - 5000) * 0.08;
+            System.out.println("Vero: " + vero);
+        } else if (lahja >= 25000 && lahja < 55000) {
+            double vero = 1700 + (lahja - 25000) * 0.10;
+            System.out.println("Vero: " + vero);
+        } else if (lahja >= 55000 && lahja < 200000) {
+            double vero = 4700 + (lahja - 55000) * 0.12;
+            System.out.println("Vero: " + vero);
+        } else if (lahja >= 200000 && lahja < 1000000) {
+            double vero = 22100 + (lahja - 200000) * 0.15;
+            System.out.println("Vero: " + vero);
         } else {
-            System.out.println("Mahdotonta!");
+            double vero = 142100 + (lahja - 1000000) * 0.17;
+            System.out.println("Vero: " + vero);
         }
     }
 }
